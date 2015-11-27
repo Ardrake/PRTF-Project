a0cfc21a76f5d651e2a0fe6729bd7d14a81c0e15
André <andrecooke@hotmail.com>
2015-11-27 10:20:14 -0500

    Classe Personne et Classe Conservateur

---
+++ SGI/Conservateur.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGI
+{
+    /// <summary>
+    ///  Classe Conservateur - hérite de Personne
+    /// </summary>
+    class Conservateur : Personne
+    {
+
+        private double commission;
+        const double TauxDeCommission = 0.10;
+
+        public double Commission
+        { 
+            get { return commission; }
+            set { commission = value; }
+        }
+
+        public Conservateur()
+        {
+        }
+
+        public Conservateur(string iDConservateur, string PrenomConservateur, string NomConservateur) : base(iDConservateur, PrenomConservateur, NomConservateur)
+        {
+            commission = 0;
+        }
+
+        public override string ToString()
+        {
+            string infoConservateur = "\nID: " + base.ID + " \nNom :" + base.Prenom + " " + base.Nom + "\nCommission payé: " + commission;
+
+            return infoConservateur;
+        }
+
+        public void SetComm(double PrixVente)
+        {
+            double commissionPayer;
+
+            if (PrixVente > 0)
+            {
+                commissionPayer = PrixVente * TauxDeCommission;
+                commission += commissionPayer;
+            }
+        }
+    }
+}
---
+++ SGI/Personne.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGI
+{
+    /// <summary>
+    /// Classe abstraite pour Personne
+    /// </summary>
+    public abstract class Personne
+    {
+        private string id = "";
+        private string prenom = "";
+        private string nom = "";
+
+        public string ID
+        {
+            get { return id; }
+            set { ID = id; }
+        }
+
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public Personne()
+        {
+        }
+
+        public Personne(string IdPersonne, string PrenomPersonne, string NomPersonne)
+        {
+            id = IdPersonne;
+            prenom = PrenomPersonne;
+            nom = NomPersonne;
+        }
+
+        public override string ToString()
+        {
+            return id + " : " + prenom + " " + nom;
+        }
+    }
+}
